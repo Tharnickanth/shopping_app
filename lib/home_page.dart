c1487aad9beaa180d76c64dfab21e07b31d25de8
Tharnickanth <tharnimo.com>
2024-02-17 07:24:56 +0530

    brand tag created

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
-class HomeUI extends StatelessWidget {
+class HomeUI extends StatefulWidget {
   const HomeUI({super.key});
+
+  @override
+  State<HomeUI> createState() => _HomeUIState();
+}
+
+class _HomeUIState extends State<HomeUI> {
+  final List<String> filters = const ["All", "Adidas", "Nike", "Bata"];
+  late String selectedFilter = filters[0];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    String selectedFilter = filters[0];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,11 +29,11 @@ class HomeUI extends StatelessWidget {
       ),
     );
 
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
-            Row(
+            const Row(
               children: [
                 Padding(
                   padding: EdgeInsets.all(20.0),
@@ -44,6 +59,41 @@ class HomeUI extends StatelessWidget {
                   ),
                 )
               ],
+            ),
+            SizedBox(
+              height: 100,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: filters.length,
+                itemBuilder: (context, index) {
+                  final filter = filters[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedFilter = filter;
+                        });
+                      },
+                      child: Chip(
+                        backgroundColor: selectedFilter == filter
+                            ? Theme.of(context).colorScheme.primary
+                            : const Color.fromRGBO(245, 247, 249, 1),
+                        label: Text(
+                          filter,
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 15),
+                        side: const BorderSide(
+                            color: Color.fromRGBO(245, 247, 249, 1)),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                      ),
+                    ),
+                  );
+                },
+              ),
             )
           ],
         ),
